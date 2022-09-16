b87d647880b9aadd7dddd49aa5c7e8dd4c8a2cc7
unknown <nimmanaprasanthlove@gmail.com>
2022-09-17 04:47:05 +0530

    empty div creates when user click on button that issue has fixed with js

--- JS-practice/jspractice.js
+++ JS-practice/jspractice.js
@@ -64,31 +64,35 @@ function register() {
         return x.email == usersEmail;
     }) // if the filter passes a data then it have some length
 
-
-    if (usercount.length == 0) {
-        let user = {
-            name: usersName,
-            email: usersEmail,
+    if (usersName != "" && usersEmail != "") {
+        if (usercount.length == 0) {
+            let user = {
+                name: usersName,
+                email: usersEmail,
+            }
+            users.push(user)
+            let generateContainer = document.getElementById('userContainer');
+            let div = document.createElement('div');
+            div.classList.add("user-card");
+            generateContainer.appendChild(div);
+            let getname = document.createElement('h3');
+            let getemail = document.createElement("p");
+            div.appendChild(getname);
+            div.appendChild(getemail);
+            getname.innerHTML = usersName;
+            getemail.innerHTML = usersEmail;
         }
-        users.push(user)
-        let generateContainer = document.getElementById('userContainer');
-        let div = document.createElement('div');
-        div.classList.add("user-card");
-        generateContainer.appendChild(div);
-        let getname = document.createElement('h3');
-        let getemail = document.createElement("p");
-        div.appendChild(getname);
-        div.appendChild(getemail);
+        else {
+            console.log("user is already exist");
+        }
+        console.log(users);
+        document.getElementById('name').value = "";  // to remove the data that lays in input field after register
+        document.getElementById('email').value = "";
+    }
+    else {
+        alert("enter user fields")
+    }
 
-        getname.innerHTML= usersName;
-        getemail.innerHTML = usersEmail;
-    }
-    else{
-        console.log("user is already exist");
-    }
-    console.log(users);
-    document.getElementById('name').value = "";  // to remove the data that lays in input field after register
-    document.getElementById('email').value = "";
 }
 
 
@@ -105,16 +109,19 @@ function register() {
 // let foralert = document.getElementsByClassName('alert-msg')[0];
 // console.log(foralert);
 
-checkdoubleentry = () => {
+
+
+
+checkdoubleentry = () => {  
     let foralert = document.getElementById("demo");
     let usersEmail = document.getElementById('email').value;
     let usercount = users.filter((x) => {
         return x.email == usersEmail;
     })
-    if (usercount.length == 0){
+    if (usercount.length == 0) {
         foralert.classList.add("alert-msg")
     }
-    else{
+    else {
         foralert.classList.remove("alert-msg")
     }
 
