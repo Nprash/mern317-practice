84479659a6cf8781c6f2c8749e0bf1b6cafd5414
unknown <nimmanaprasanthlove@gmail.com>
2022-10-15 10:10:15 +0530

    qotes app final stage

--- Quotes App/app.js
+++ Quotes App/app.js
@@ -1,27 +1,61 @@
 
+let randomauthors = [];
 
 fetchrandomQuote = () => {
-    let randomnumber = Math.floor(Math.random() * 1000)
+    randomauthors = [];
 
     fetch("https://type.fit/api/quotes")
         .then(function (response) {
             return response.json();
         })
         .then(function (quote) {
+            let randomnumber = Math.floor(Math.random() * quote.length);
+
             //console.log(quote[randomnumber].text, quote[randomnumber].author); //bcz its an array so it contains index positions so we used square bracket to fetch the individual quote
             document.getElementById("Rquote").innerHTML = quote[randomnumber].text;
             document.getElementById("author").innerHTML = quote[randomnumber].author
+            randomauthors.push((quote[randomnumber].author)) // we want 3 author names on the screen so we used 3 times push over here
+            randomauthors.push((quote[randomnumber + 3].author))
+            randomauthors.push((quote[randomnumber + 6].author))
+            // console.log(randomauthors);
+
+            //to get authors names on the screen
+
+            let authorscontainer = document.getElementById("authors-container");
+            authorscontainer.innerHTML = "";
+            randomauthors.map((x) => {
+                if (x != null) {
+                    let author = document.createElement('div');
+                    author.classList.add("author");
+                    author.addEventListener('click', renderRequestedQuotes) // when we click on any of author name we will get quotes 
+                    author.innerHTML = x;
+                    authorscontainer.appendChild(author);
+                }
+            })
+
         });
 
-    let maincard = document.getElementById("cardid");
-
-
+    // let maincard = document.getElementById("cardid");
 
 }
 
 fetchrandomQuote();
 
+renderRequestedQuotes = (event) => {
+    console.log(event.target.innerHTML);
+    let requestedauthor = event.target.innerHTML;
 
+    fetch("https://type.fit/api/quotes")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (quote) {
+            let requestedquote = quote.filter((y) => {
+                return y.author == requestedauthor; // wwe aare filtering those author names
+            })
+            console.log(requestedquote)
+        })
+}
 
 
 
